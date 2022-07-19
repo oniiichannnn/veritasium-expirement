eb46a367a99af5b6a1247aad5d68738c062c7811
potsu <67128188+oniiichannnn@users.noreply.github.com>
2022-07-19 23:11:56 +0800

    Create start.js

---
+++ start.js
@@ -0,0 +1,78 @@
+const simulations =  5000;
+
+let success = 0;
+let total_found_prisoners = 0;
+
+loop(start_simulation, simulations)
+
+
+function start_simulation (i) {
+ const numbers = [];
+ const boxes = [];
+
+ let found_number_prisoners = 0;
+
+ loop(function (i) { numbers.push(i) }, 100);
+
+ let generated_contents = [];
+ loop(function (i) {
+	const label = i;
+	const filtered_contents = numbers.filter(n => !generated_contents.includes(n));
+	const content = filtered_contents[Math.floor(Math.random() * filtered_contents.length)];
+	
+	boxes.push({ label, content });
+ }, 100)
+
+ loop(function (i) {
+	const prisoner = i;
+	const opened_boxes = [];
+
+	let previous_box_content
+
+	loop(function () {
+		const filtered_boxes = boxes.filter(box => !opened_boxes.includes(box));
+		let chosen_box;
+
+		switch (method) {
+			case "METHOD_1": {
+				chosen_box = filtered_boxes[Math.floor(Math.random() * filtered_boxes.length)]
+			} break;
+
+			case "METHOD_2": {
+				chosen_box = function () {
+					if (previous_box_content !== undefined) {
+						if (opened_boxes.find(box => box.label === previous_box_content)) {
+					
+							previous_box_content = undefined;
+							return filtered_boxes[Math.floor(Math.random() * filtered_boxes.length)]
+						
+						} else return filtered_boxes.find(box => box.label === previous_box_content)
+					} else return filtered_boxes[Math.floor(Math.random() * filtered_boxes.length)]
+				}();
+			} break;
+		}
+
+		previous_box_content = chosen_box.content;
+
+		opened_boxes.push(chosen_box)
+
+		if (chosen_box.content === prisoner) { found_number_prisoners++ ; return "break" }
+	}, 50)
+ }, 100)
+
+ total_found_prisoners += found_number_prisoners;
+
+ if (found_number_prisoners === 100) {
+	success++ ; console.log(`Simulation: ${i + 1} Result: SUCCESS Found: ${found_number_prisoners}`)
+ } else return console.log(`Simulation: ${i + 1} Result: FAILED Found: ${found_number_prisoners}`)
+}
+
+function loop (callback, times) {
+  for (let i = 0 ; i < times ; i++) {
+    	const result = callback(i);
+
+	if (result === "break") break
+  }
+}
+
+console.log(`Success: ${success} ; Fails: ${simulations - success} ; Total Found: ${total_found_prisoners}/${100 * simulations} ; AVG Found: ${total_found_prisoners / simulations}`)
